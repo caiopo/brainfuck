191c32b5b622dc7ca70193a2ee80eda3f029dc81
caiopo <caiopoliveira@gmail.com>
2016-06-10 14:44:58 -0300

    Add Java

--- Makefile
+++ Makefile
@@ -2,7 +2,13 @@ nim:
 	nim c -d:release brainfuck.nim
 
 cpp:
-	g++ brainfuck.cpp -o brainfuck -std=c++11 -Wall -pedantic -Wextra
+	g++ brainfuck.cpp -o brainfuck -std=c++11 -Wall -pedantic -Wextra -O3
 
 go:
 	go build brainfuck.go
+
+java:
+	javac brainfuck.java
+
+clean:
+	rm brainfuck{,.class}
---
+++ brainfuck.java
@@ -0,0 +1,92 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;;
+
+public class brainfuck {
+
+	static int codePos = 0, tapePos = 0;
+
+	static ArrayList<Character> tape = new ArrayList<Character>(300);
+
+	static String code;
+
+	static Scanner scanner;
+
+	public static void main(String[] args) {
+		scanner = new Scanner(System.in);
+
+		System.out.println(args[0]);
+
+		if (args.length > 0) {
+
+			byte[] bytes = null;
+
+			try {
+				bytes = Files.readAllBytes(Paths.get(args[0]));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			code = new String(bytes, StandardCharsets.UTF_8);
+		} else {
+			System.out.println("error: reading code from stdin not implemented yet");
+			System.exit(1);
+		}
+
+		run(false);
+
+	}
+
+	public static boolean run(boolean skip) {
+		while (codePos < code.length()) {
+			if (tapePos >= tape.size())
+				tape.add('\0');
+
+			if (code.charAt(codePos) == '[') {
+				++codePos;
+				int oldPos = codePos;
+				while (run(tape.get(tapePos) == '\0'))
+					codePos = oldPos;
+			} else if (code.charAt(codePos) == ']') {
+				return tape.get(tapePos) != '\0';
+			} else if (!skip) {
+				switch (code.charAt(codePos)) {
+				case '+':
+					tape.set(tapePos, (char) (tape.get(tapePos) + 1));
+					break;
+
+				case '-':
+					tape.set(tapePos, (char) (tape.get(tapePos) - 1));
+					break;
+
+				case '>':
+					++tapePos;
+					break;
+
+				case '<':
+					--tapePos;
+					break;
+
+				case '.':
+					System.out.print(tape.get(tapePos));
+					break;
+
+				case ',':
+					try {
+						tape.set(tapePos, (char) System.in.read());
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					break;
+				}
+			}
+
+			++codePos;
+		}
+
+		return false;
+
+	}
+}
